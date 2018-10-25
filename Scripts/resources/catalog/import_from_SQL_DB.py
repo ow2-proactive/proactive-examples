9c7307295eef16314a1550299e8ab3ae7e400826
Aminelouati <louati.amiine@gmail.com>
2018-10-25 18:43:42 +0200

    improve data connectors workflows

--- Scripts/resources/catalog/import_from_SQL_DB.py
+++ Scripts/resources/catalog/import_from_SQL_DB.py
@@ -70,7 +70,7 @@ engine = create_engine(database_url)
 with engine.connect() as conn, conn.begin():
     #pd.read_sql() can take either a SQL query as a parameter or a table name
     dataframe = pd.read_sql(SQL_QUERY, conn)
-
+print(dataframe.to_string())
 #***************# HTML PREVIEW STYLING #***************#
 styles = [
     dict(selector="th", props=[("font-weight", "bold"),
@@ -88,12 +88,14 @@ styles = [
 #******************************************************#
 
 if OUTPUT_TYPE == "HTML":
+    print('The task result will be previewed in HTML format')
     result = dataframe.style.set_table_styles(styles).render().encode('utf-8')
     resultMetadata.put("file.extension", ".html")
     resultMetadata.put("file.name", "output.html")
     resultMetadata.put("content.type", "text/html")
 else:
     # Write results to the task result in CSV format
+    print('The task result will be written in csv file')
     result = dataframe.to_csv(index=False).encode('utf-8')
     resultMetadata.put("file.extension", ".csv")
     resultMetadata.put("file.name", "result.csv")
