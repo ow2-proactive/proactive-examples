f4c350a4f897853350b3d3fe1b478caaaef7e990
Aminelouati <louati.amiine@gmail.com>
2018-11-29 14:24:17 +0100

    apply reviewer comments

--- DataConnectorScripts/resources/catalog/ExportToFileSystem.groovy
+++ DataConnectorScripts/resources/catalog/ExportToFileSystem.groovy
@@ -18,7 +18,7 @@ import org.objectweb.proactive.extensions.dataspaces.vfs.selector.*
 URI_SCHEME = args[0]
 
 //Set connection parameters and retrieve the SFTP/FTP password
-final String URL_KEY = URI_SCHEME+"://<username>@<host>";
+final String URL_KEY = URI_SCHEME + "://<username>@<host>";
 host = variables.get("HOST")
 username = variables.get("USERNAME")
 port = variables.get("PORT")
@@ -26,7 +26,7 @@ password = checkParametersAndReturnPassword()
 
 //Initialize the connection manger to the remote SFTP/FTP server.
 optsRemote = new FileSystemOptions()
-fsManager = null 
+fsManager = null
 initializeAuthentication()
 
 //Initialize file pattern, local and remote bases
@@ -43,64 +43,65 @@ exportFiles()
 release()
 
 /**
-* Retrieves files that match the specified File pattern from the local directory (data space)
-* and export them to the SFTP/FTP server.
-*/
-void exportFiles(){
-	try {
-    	optsLocal = new FileSystemOptions()
-        if (port == null || port.isEmpty()){
-      		startRemoteUrl = URI_SCHEME+"://" + host + "/" + remoteDir
-        } else{
-            startRemoteUrl = URI_SCHEME+"://" + host + ":" + port + "/" + remoteDir
+ * Retrieves files that match the specified File pattern from the local directory (data space)
+ * and export them to the SFTP/FTP server.
+ */
+void exportFiles() {
+    try {
+        optsLocal = new FileSystemOptions()
+        if (port == null || port.isEmpty()) {
+            startRemoteUrl = URI_SCHEME + "://" + host + "/" + remoteDir
+        } else {
+            startRemoteUrl = URI_SCHEME + "://" + host + ":" + port + "/" + remoteDir
         }
         // Set remoteSrc for cleanup in release()
         remoteSrc = fsManager.resolveFile(startRemoteUrl, optsRemote);
-        if(Paths.get(localBase).isAbsolute()){
+        // localBase can be either a global path or a local relative path in the data space
+        if (Paths.get(localBase).isAbsolute()) {
             localDir = localBase
-        } else{
+        } else {
             localDir = Paths.get(localspace, localBase).toString()
         }
-    	startLocalPath = new File(localDir).toURI().toURL().toString();  
-    	println "Local path is : " + startLocalPath
-    	(new File(localDir)).mkdirs()
+        startLocalPath = new File(localDir).toURI().toURL().toString();
+        println "Local path is : " + startLocalPath
+        (new File(localDir)).mkdirs()
         localFileRoot = fsManager.resolveFile(startLocalPath, optsLocal)
         // Set src for cleanup in release()
         src = localFileRoot
-		localBasePath = localFileRoot.getName()
-		children = localFileRoot.findFiles(new org.objectweb.proactive.extensions.dataspaces.vfs.selector.FileSelector(filePattern))
-		children.each { f ->
-        	String relativePath = localBasePath.getRelativeName(f.getName());
-			if (f.getType() == FileType.FILE) {
-            	println("Examining local file " + f.getName());
-            	String remoteUrl = startRemoteUrl + "/" + relativePath;
-            	println("  Remote url is " + remoteUrl);
-            	remoteFile = fsManager.resolveFile(remoteUrl, optsRemote);
-            	println("Resolved remote file name: " + remoteFile.getName());
-            	if (!remoteFile.getParent().exists()) {
-                	if(!remoteFile.getParent().isWriteable()){
-                   		 throw new RuntimeException("This file " + remoteFile.getParent() + " is read-only")
-                	}
+        localBasePath = localFileRoot.getName()
+        children = localFileRoot.findFiles(new org.objectweb.proactive.extensions.dataspaces.vfs.selector.FileSelector(filePattern))
+        children.each { f ->
+            String relativePath = localBasePath.getRelativeName(f.getName());
+            if (f.getType() == FileType.FILE) {
+                println("Examining local file " + f.getName());
+                String remoteUrl = startRemoteUrl + "/" + relativePath;
+                println("  Remote url is " + remoteUrl);
+                remoteFile = fsManager.resolveFile(remoteUrl, optsRemote);
+                println("Resolved remote file name: " + remoteFile.getName());
+                if (!remoteFile.getParent().exists()) {
+                    if (!remoteFile.getParent().isWriteable()) {
+                        throw new RuntimeException("This folder " + remoteFile.getParent() + " is read-only")
+                    }
                     remoteFile.getParent().createFolder();
                     println("create the remote folder " + remoteFile.getParent().toString())
-            	}
-            	println("  ### Uploading file ###");
-                if(!remoteFile.isWriteable()){
-                   		 throw new RuntimeException("This file " + remoteFile + " is read-only")
                 }
-            	remoteFile.copyFrom(f, new AllFileSelector());
-			} else {
-            	println("Ignoring non-file " + f.getName());
-       		 } 
+                println("  ### Uploading file ###");
+                if (!remoteFile.isWriteable()) {
+                    throw new RuntimeException("This file " + remoteFile + " is read-only")
+                }
+                remoteFile.copyFrom(f, new AllFileSelector());
+            } else {
+                println("Ignoring non-file " + f.getName());
+            }
         }
-	} catch (FileSystemException ex) {
-            throw new RuntimeException(ex);
+    } catch (FileSystemException ex) {
+        throw new RuntimeException(ex);
     }
 }
 
 /**
-* Release system resources, close connections to the local and remote the filesystems. 
-*/
+ * Release system resources, close connections to the local and remote the filesystems.
+ */
 void release() {
     FileSystem fs = null;
     FileSystem rfs = null;
@@ -109,50 +110,50 @@ void release() {
         fs = src.getFileSystem() // This works even if the src is closed.
         fsManager.closeFileSystem(fs)
     }
-    if(remoteSrc != null){
+    if (remoteSrc != null) {
         remoteSrc.close()
         rfs = remoteSrc.getFileSystem()
-    	fsManager.closeFileSystem(rfs)
-    }    
-} 
+        fsManager.closeFileSystem(rfs)
+    }
+}
 
 /**
-* Checks whether the provided host, username and port values are empty or not, then
-* returns the SFTP/FTP password using the third party credentials mechanism
-*/
-def checkParametersAndReturnPassword (){
-    if (host.isEmpty()){
-    	throw new IllegalArgumentException("ERROR: HOST variable is not provided by the user. Empty value is not allowed.")
-	}
-	if (username.isEmpty()){
-     	throw new IllegalArgumentException("ERROR: USERNAME variable is not provided by the user. Empty value is not allowed.")
-	}
-	def urlKey = URI_SCHEME+"://" + username + "@" + host;
-	def password = credentials.get(urlKey)
-	if (password == null || password.isEmpty()){
-   		 throw new IllegalArgumentException("Please add your " + URI_SCHEME + " password to 3rd-party credentials under the key :\"" +
-                                               URL_KEY + "\"");
-	}
+ * Checks whether the provided host, username and port values are empty or not, then
+ * returns the SFTP/FTP password using the third party credentials mechanism
+ */
+def checkParametersAndReturnPassword() {
+    if (host.isEmpty()) {
+        throw new IllegalArgumentException("ERROR: HOST variable is not provided by the user. Empty value is not allowed.")
+    }
+    if (username.isEmpty()) {
+        throw new IllegalArgumentException("ERROR: USERNAME variable is not provided by the user. Empty value is not allowed.")
+    }
+    def urlKey = URI_SCHEME + "://" + username + "@" + host;
+    def password = credentials.get(urlKey)
+    if (password == null || password.isEmpty()) {
+        throw new IllegalArgumentException("Please add your " + URI_SCHEME + " password to 3rd-party credentials under the key :\"" +
+                URL_KEY + "\"");
+    }
     return password
 }
 
 /**
-* Initializes the connection to the remote SFTP/FTP server 
-* Returns the FileSystemManager instance that manages this connection.
-*/
-void initializeAuthentication(){
+ * Initializes the connection to the remote SFTP/FTP server
+ * Returns the FileSystemManager instance that manages this connection.
+ */
+void initializeAuthentication() {
     try {
-    	fsManager = VFS.getManager();
-	} catch (FileSystemException ex) {
-    	throw new RuntimeException("failed to get fsManager from VFS", ex);
-	}
-	def auth = new StaticUserAuthenticator(null, username, password)
-	try {
- 		DefaultFileSystemConfigBuilder.getInstance().setUserAuthenticator(optsRemote, auth);
+        fsManager = VFS.getManager();
+    } catch (FileSystemException ex) {
+        throw new RuntimeException("failed to get fsManager from VFS", ex);
+    }
+    def auth = new StaticUserAuthenticator(null, username, password)
+    try {
+        DefaultFileSystemConfigBuilder.getInstance().setUserAuthenticator(optsRemote, auth);
         FtpFileSystemConfigBuilder.getInstance().setPassiveMode(optsRemote, true);
-	} catch (FileSystemException ex) {
-    	throw new RuntimeException("setUserAuthenticator failed", ex);
-	}
+    } catch (FileSystemException ex) {
+        throw new RuntimeException("setUserAuthenticator failed", ex);
+    }
 }
 
 result = true
--- DataConnectorScripts/resources/catalog/ImportFromFileSystem.groovy
+++ DataConnectorScripts/resources/catalog/ImportFromFileSystem.groovy
@@ -12,12 +12,13 @@ import org.apache.commons.vfs2.*;
 import org.apache.commons.vfs2.auth.*;
 import org.apache.commons.vfs2.impl.*;
 import org.apache.commons.vfs2.provider.local.*;
+import org.apache.commons.vfs2.provider.ftp.FtpFileSystemConfigBuilder;
 import org.objectweb.proactive.extensions.dataspaces.vfs.selector.*
 
 URI_SCHEME = args[0]
 
 ///Set connection parameters and retrieve the SFTP/FTP password
-final String URL_KEY = URI_SCHEME+"://<username>@<host>";
+final String URL_KEY = URI_SCHEME + "://<username>@<host>";
 host = variables.get("HOST")
 username = variables.get("USERNAME")
 port = variables.get("PORT")
@@ -25,7 +26,7 @@ password = checkParametersAndReturnPassword()
 
 //Initialize the connection manger to the remote SFTP/FTP server.
 optsRemote = new FileSystemOptions()
-fsManager = null 
+fsManager = null
 initializeAuthentication()
 
 //Initialize file pattern, local and remote bases
@@ -42,62 +43,63 @@ importFiles()
 release()
 
 /**
-* Retrieves files that match the specified File pattern from the SFTP/FTP server
-* and import them to the local directory (data space).
-*/
-void importFiles(){
-	try {   
-    	if (port == null || port.isEmpty()){
-      		startUrl = URI_SCHEME+"://" + host + "/" + remoteDir
-        } else{
-            startUrl = URI_SCHEME+"://" + host + ":" + port + "/" + remoteDir
+ * Retrieves files that match the specified File pattern from the SFTP/FTP server
+ * and import them to the local directory (data space).
+ */
+void importFiles() {
+    try {
+        if (port == null || port.isEmpty()) {
+            startUrl = URI_SCHEME + "://" + host + "/" + remoteDir
+        } else {
+            startUrl = URI_SCHEME + "://" + host + ":" + port + "/" + remoteDir
         }
-    	if(Paths.get(localBase).isAbsolute()){
+        // localBase can be either a global path or a local relative path in the data space
+        if (Paths.get(localBase).isAbsolute()) {
             localDir = localBase
-        } else{
+        } else {
             localDir = Paths.get(localspace, localBase).toString()
         }
         // Set remoteSrc for cleanup in release()
         remoteSrc = fsManager.resolveFile(localDir);
         (new File(localDir)).mkdirs()
-        ftpFile = fsManager.resolveFile(startUrl, optsRemote)
+        remoteFile = fsManager.resolveFile(startUrl, optsRemote)
         // Set src for cleanup in release()
-        src = ftpFile
-        remoteBasePath = ftpFile.getName()
-        children = this.ftpFile.findFiles(new org.objectweb.proactive.extensions.dataspaces.vfs.selector.FileSelector(filePattern))
+        src = remoteFile
+        remoteBasePath = remoteFile.getName()
+        children = this.remoteFile.findFiles(new org.objectweb.proactive.extensions.dataspaces.vfs.selector.FileSelector(filePattern))
         children.each { f ->
-        	String relativePath = File.separator + remoteBasePath.getRelativeName(f.getName());
-       		 if (f.getType() == FileType.FILE) {
-            	println("Examining remote file " + f.getName());
+            String relativePath = File.separator + remoteBasePath.getRelativeName(f.getName());
+            if (f.getType() == FileType.FILE) {
+                println("Examining remote file " + f.getName());
                 standardPath = new File(localDir, relativePath);
-            	localUrl = standardPath.toURI().toURL();            
-            	println("  Standard local path is " + standardPath);
-            	LocalFile localFile = (LocalFile) fsManager.resolveFile(localUrl.toString());
-            	println("    Resolved local file name: " + localFile.getName());
+                localUrl = standardPath.toURI().toURL();
+                println("  Standard local path is " + standardPath);
+                LocalFile localFile = (LocalFile) fsManager.resolveFile(localUrl.toString());
+                println("    Resolved local file name: " + localFile.getName());
                 if (!localFile.getParent().exists()) {
-                	if(!localFile.getParent().isWriteable()){
-                   		 throw new RuntimeException("This file " + localFile.getParent() + " is read-only")
-                	}
+                    if (!localFile.getParent().isWriteable()) {
+                        throw new RuntimeException("This folder " + localFile.getParent() + " is read-only")
+                    }
                     localFile.getParent().createFolder();
                     println("create the local folder " + localFile.getParent().toString())
-            	}
-            	println("  ### Retrieving file ###");
-                if(!localFile.isWriteable()){
-                   		 throw new RuntimeException("This file " + localFile + " is read-only")
                 }
-            	localFile.copyFrom(f, new AllFileSelector());
-             } else {
-            	println("Ignoring non-file " + f.getName());
-        	}
-    	}
-	} catch (FileSystemException ex) {
-            throw new RuntimeException(ex);
+                println("  ### Retrieving file ###");
+                if (!localFile.isWriteable()) {
+                    throw new RuntimeException("This file " + localFile + " is read-only")
+                }
+                localFile.copyFrom(f, new AllFileSelector());
+            } else {
+                println("Ignoring non-file " + f.getName());
+            }
+        }
+    } catch (FileSystemException ex) {
+        throw new RuntimeException(ex);
     }
 }
 
 /**
-* Release system resources, close connections to the local and remote filesystems. 
-*/
+ * Release system resources, close connections to the local and remote filesystems.
+ */
 void release() {
     FileSystem fs = null;
     FileSystem rfs = null;
@@ -106,49 +108,51 @@ void release() {
         fs = src.getFileSystem() // This works even if the src is closed.
         fsManager.closeFileSystem(fs)
     }
-    if(remoteSrc != null){
+    if (remoteSrc != null) {
         remoteSrc.close()
         rfs = remoteSrc.getFileSystem()
-    	fsManager.closeFileSystem(rfs)
-    }    
-} 
+        fsManager.closeFileSystem(rfs)
+    }
+}
 
 /**
-* Checks whether the provided host, username and port values are empty or not, then
-* returns the SFTP/FTP password using the third party credentials mechanism
-*/
-def checkParametersAndReturnPassword (){
-    if (host.isEmpty()){
-    	throw new IllegalArgumentException("ERROR: HOST variable is not provided by the user. Empty value is not allowed.")
-	}
-	if (username.isEmpty()){
-     	throw new IllegalArgumentException("ERROR: USERNAME variable is not provided by the user. Empty value is not allowed.")
-	}
-	def urlKey = URI_SCHEME+"://" + username + "@" + host;
-	def password = credentials.get(urlKey)
-	if (password == null || password.isEmpty()){
-   		 throw new IllegalArgumentException("Please add your " + URI_SCHEME + " password to 3rd-party credentials under the key :\"" +
-                                               URL_KEY + "\"");
-	}
+ * Checks whether the provided host, username and port values are empty or not, then
+ * returns the SFTP/FTP password using the third party credentials mechanism
+ */
+def checkParametersAndReturnPassword() {
+    if (host.isEmpty()) {
+        throw new IllegalArgumentException("ERROR: HOST variable is not provided by the user. Empty value is not allowed.")
+    }
+    if (username.isEmpty()) {
+        throw new IllegalArgumentException("ERROR: USERNAME variable is not provided by the user. Empty value is not allowed.")
+    }
+    def urlKey = URI_SCHEME + "://" + username + "@" + host;
+    def password = credentials.get(urlKey)
+    if (password == null || password.isEmpty()) {
+        throw new IllegalArgumentException("Please add your " + URI_SCHEME + " password to 3rd-party credentials under the key :\"" +
+                URL_KEY + "\"");
+    }
     return password
 }
 
 /**
-* Initializes the connection to the remote SFTP/FTP server 
-* Returns the FileSystemManager instance that manages this connection.
-*/
-void initializeAuthentication(){
+ * Initializes the connection to the remote SFTP/FTP server
+ * Returns the FileSystemManager instance that manages this connection.
+ */
+void initializeAuthentication() {
     try {
-    	fsManager = VFS.getManager();
-	} catch (FileSystemException ex) {
-    	throw new RuntimeException("failed to get fsManager from VFS", ex);
-	}
-	def auth = new StaticUserAuthenticator(null, username, password)
-	try {
- 		DefaultFileSystemConfigBuilder.getInstance().setUserAuthenticator(optsRemote, auth);
-	} catch (FileSystemException ex) {
-    	throw new RuntimeException("setUserAuthenticator failed", ex);
-	}
+        fsManager = VFS.getManager();
+    } catch (FileSystemException ex) {
+        throw new RuntimeException("failed to get fsManager from VFS", ex);
+    }
+    def auth = new StaticUserAuthenticator(null, username, password)
+    try {
+
+        DefaultFileSystemConfigBuilder.getInstance().setUserAuthenticator(optsRemote, auth);
+        FtpFileSystemConfigBuilder.getInstance().setPassiveMode(optsRemote, true);
+    } catch (FileSystemException ex) {
+        throw new RuntimeException("setUserAuthenticator failed", ex);
+    }
 }
 
 result = true
