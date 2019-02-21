3929de6485478c11acedab79092ee7feaf185798
Caroline Pacheco do Espirito Silva <lolyne.pacheco@gmail.com>
2019-02-21 21:29:55 +0100

    Add deep learning scripts (#379)

---
+++ DeepLearningScripts/resources/catalog/DenseNet_161.py
@@ -0,0 +1,52 @@
+print("BEGIN DesNet_161")
+
+import json
+
+USE_PRETRAINED_MODEL = 'true'
+if 'variables' in locals():
+  if variables.get("USE_PRETRAINED_MODEL") is not None:
+    USE_PRETRAINED_MODEL = str(variables.get("USE_PRETRAINED_MODEL")).lower()
+
+pretrained = False
+if USE_PRETRAINED_MODEL == 'true':
+  pretrained = True
+
+# Define the CNN model
+CNN_MODEL = """
+cnn = models.densenet161(pretrained=""" + str(pretrained) + """)
+cnn.features = nn.Sequential(*list(cnn.children())[:-1])
+cnn.classifier = nn.Linear(2208, num_classes)
+"""
+print(CNN_MODEL)
+
+# Data augmentation and normalization for training
+# Just normalization for validation and test
+CNN_TRANSFORM = """
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+"""
+print(CNN_TRANSFORM)
+
+if 'variables' in locals():
+  variables.put("CNN_MODEL", CNN_MODEL)
+  variables.put("CNN_TRANSFORM", CNN_TRANSFORM)
+
+print("END DesNet_161")
---
+++ DeepLearningScripts/resources/catalog/FCN.py
@@ -0,0 +1,173 @@
+print("BEGIN FCN")
+
+import json
+from ast import literal_eval as make_tuple
+
+IMG_SIZE = variables.get("IMG_SIZE")
+NUM_CLASSES = int(str(variables.get("NUM_CLASSES")))
+ 
+IMG_SIZE = make_tuple(IMG_SIZE)
+
+if NUM_CLASSES == 2:
+    NUM_CLASSES = NUM_CLASSES - 1       
+else:
+    NUM_CLASSES = NUM_CLASSES + 2   
+
+# Define the NET model
+NET_MODEL = """
+
+class FCN16(nn.Module):
+
+    def __init__(self, num_classes):
+        super().__init__()
+
+        feats = list(models.vgg16(pretrained=True).features.children())
+        self.feats = nn.Sequential(*feats[0:16])
+        self.feat4 = nn.Sequential(*feats[17:23])
+        self.feat5 = nn.Sequential(*feats[24:30])
+        self.fconn = nn.Sequential(
+            nn.Conv2d(512, 4096, 7),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 4096, 1),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+        )
+        self.score_fconn = nn.Conv2d(4096, num_classes, 1)
+        self.score_feat4 = nn.Conv2d(512, num_classes, 1)
+
+    def forward(self, x):
+        feats = self.feats(x)
+        feat4 = self.feat4(feats)
+        feat5 = self.feat5(feat4)
+        fconn = self.fconn(feat5)
+
+        score_feat4 = self.score_feat4(feat4)
+        score_fconn = self.score_fconn(fconn)
+
+        score = F.upsample_bilinear(score_fconn, score_feat4.size()[2:])
+        score += score_feat4
+
+        return F.upsample_bilinear(score, x.size()[2:])
+        
+        
+METHOD_NAME = 'fcn16'        
+Net = FCN16
+assert Net is not None, f'model {MODEL_NAME} not available'
+model = Net(NUM_CLASSES)
+        
+"""
+print(NET_MODEL)
+
+# Data augmentation and normalization for training
+# Just normalization for validation and test
+NET_TRANSFORM = """
+import numpy as np
+def colormap(n):
+    cmap=np.zeros([n, 3]).astype(np.uint8)
+
+    for i in np.arange(n):
+        r, g, b = np.zeros(3)
+
+        for j in np.arange(8):
+            r = r + (1<<(7-j))*((i&(1<<(3*j))) >> (3*j))
+            g = g + (1<<(7-j))*((i&(1<<(3*j+1))) >> (3*j+1))
+            b = b + (1<<(7-j))*((i&(1<<(3*j+2))) >> (3*j+2))
+
+        cmap[i,:] = np.array([r, g, b])
+
+    return cmap
+
+class Relabel:
+
+    def __init__(self, olabel, nlabel):
+        self.olabel = olabel
+        self.nlabel = nlabel
+
+    def __call__(self, tensor):
+        assert isinstance(tensor, torch.LongTensor), 'tensor needs to be LongTensor'
+        tensor[tensor == self.olabel] = self.nlabel
+        return tensor
+
+
+class ToLabel:
+
+    def __call__(self, image):
+        return torch.from_numpy(np.array(image)).long().unsqueeze(0)
+
+
+class Colorize:
+
+    def __init__(self, n=22):
+        self.cmap = colormap(256)
+        self.cmap[n] = self.cmap[-1]
+        self.cmap = torch.from_numpy(self.cmap[:n])
+
+    def __call__(self, gray_image):
+        size = gray_image.size()
+        color_image = torch.ByteTensor(3, size[1], size[2]).fill_(0)
+
+        for label in range(1, len(self.cmap)):
+            mask = gray_image[0] == label
+
+            color_image[0][mask] = self.cmap[label][0]
+            color_image[1][mask] = self.cmap[label][1]
+            color_image[2][mask] = self.cmap[label][2]
+
+        return color_image
+        
+if NUM_CLASSES == 1:
+    input_transform = Compose([Resize(IMG_SIZE),ToTensor()])
+    target_transform = Compose([Resize(IMG_SIZE),ToTensor()])
+else:
+    color_transform = Colorize()
+    image_transform = ToPILImage()
+    input_transform = Compose([
+            Resize(IMG_SIZE),   
+            ToTensor(),
+            Normalize([.485, .456, .406], [.229, .224, .225]),
+    ])
+    target_transform = Compose([
+            Resize(IMG_SIZE),     
+            ToLabel(),
+            Relabel(255, 21),
+    ])
+"""
+print(NET_TRANSFORM)
+
+#CRITERION FUNCTION
+NET_CRITERION = """
+class CrossEntropyLoss2d(nn.Module):
+
+    def __init__(self, weight=None):
+        super().__init__()
+
+        self.loss = nn.NLLLoss2d(weight)
+
+    def forward(self, outputs, targets):
+        return self.loss(F.log_softmax(outputs), targets)
+
+class BinaryCrossEntropyLoss2d(nn.Module):
+
+    def __init__(self, weight=None):
+        super().__init__()
+        
+        #self.loss = nn.BCELoss(weight)
+        self.loss = nn.BCEWithLogitsLoss(weight)
+
+    def forward(self, outputs, targets):
+        #return self.loss(nn.Sigmoid(outputs), targets)
+        return self.loss(outputs, targets)
+        
+"""
+print(NET_CRITERION)
+
+
+if 'variables' in locals():
+  variables.put("NET_MODEL", NET_MODEL)
+  variables.put("NET_TRANSFORM", NET_TRANSFORM)
+  variables.put("NET_CRITERION", NET_CRITERION)
+  variables.put("IMG_SIZE", IMG_SIZE)
+  variables.put("NUM_CLASSES", NUM_CLASSES)
+
+print("END FCN")
