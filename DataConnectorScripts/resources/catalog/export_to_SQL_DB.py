33821cc984b681039378e9aa3dc8c969f5bb3668
aminelouati <louati.amiine@gmail.com>
2019-09-06 12:01:08 +0200

    upgrade data connectors wfs using Workflow Variable Type PA:CREDENTIAL

--- DataConnectorScripts/resources/catalog/export_to_SQL_DB.py
+++ DataConnectorScripts/resources/catalog/export_to_SQL_DB.py
@@ -4,73 +4,73 @@ from sqlalchemy import create_engine
 import sys
 
 DBMS_NAME = args[0]
-if DBMS_NAME == "PostgreSQL":
-    DBMS_DATA=('postgresql','psycopg2',5432,"POSTGRES_")
-if DBMS_NAME == "MySQL":
-    DBMS_DATA=('mysql','mysqlconnector',3306,"MYSQL_")
-if DBMS_NAME == "Greenplum":
-    DBMS_DATA=('postgresql','psycopg2',5432,"GPDB_")
-if DBMS_NAME == "SQL Server":
-    DBMS_DATA=('sqlserver','pyodbc',1433,"SQL_SERVER_")
-if DBMS_NAME == "Oracle":
-    DBMS_DATA=('oracle','cx_oracle',1521,"ORACLE_")
+if DBMS_NAME == "postgresql":
+    DBMS_DATA=('psycopg2', 5432)
+if DBMS_NAME == "mysql":
+    DBMS_DATA=('mysqlconnector', 3306)
+if DBMS_NAME == "greenplum":
+    DBMS_DATA=('psycopg2', 5432)
+if DBMS_NAME == "sqlserver":
+    DBMS_DATA=('pyodbc', 1433)
+if DBMS_NAME == "oracle":
+    DBMS_DATA=('cx_oracle', 1521)
 
-DBMS_PROTOCOL, DBMS_DRIVER, DBMS_DEFAULT_PORT, DBMS_PREFIX = DBMS_DATA
+DBMS_DRIVER, DBMS_DEFAULT_PORT = DBMS_DATA
 
 print("BEGIN Export_Data to " + DBMS_NAME + " database using " + DBMS_DRIVER + " driver")
 
-CREDENTIALS_KEY_MSG = DBMS_PROTOCOL + "://<username>@<host>:<port>"
+CREDENTIALS_KEY_MSG = DBMS_NAME + "://<username>@<host>:<port>"
 
-HOST = variables.get(DBMS_PREFIX + "HOST")
-PORT = int(variables.get(DBMS_PREFIX + "PORT"))
-DATABASE = variables.get(DBMS_PREFIX + "DATABASE")
-USER = variables.get(DBMS_PREFIX + "USER")
+HOST = variables.get("HOST")
+PORT = variables.get("PORT")
+DATABASE = variables.get("DATABASE")
+USER = variables.get("USER")
 
 # This key is used for getting the password from 3rd party credentials.
-CREDENTIALS_KEY = DBMS_PROTOCOL + "://" + USER + "@" + HOST + ":" + str(PORT)
+CREDENTIALS_KEY = variables.get("CREDENTIALS_KEY")
 PASSWORD=credentials.get(CREDENTIALS_KEY)
 
-SQL_TABLE = variables.get(DBMS_PREFIX + "TABLE")
+SQL_TABLE = variables.get("TABLE")
 INPUT_FILE = variables.get("INPUT_FILE")
 INSERT_MODE = variables.get("INSERT_MODE")
 
 if not HOST:
-    print("ERROR: {0}HOST variable is not provided by the user.".format(DBMS_PREFIX))
+    print("ERROR: HOST variable is not provided by the user.")
     sys.exit(1)
 if not PORT:
     PORT = DBMS_DEFAULT_PORT
-    print("WARNING: {0}PORT variable is not provided by the user. Using the default value: {1}".format(DBMS_PREFIX, DBMS_DEFAULT_PORT))
+    print("WARNING: PORT variable is not provided by the user. Using the default value: {0}".format(DBMS_DEFAULT_PORT))
 if not DATABASE:
-    print("ERROR: {0}DATABASE variable is not provided by the user.".format(DBMS_PREFIX))
+    print("ERROR: DATABASE variable is not provided by the user.")
     sys.exit(1)
 if not USER:
-    print("ERROR: {0}USER variable is not provided by the user.".format(DBMS_PREFIX))
+    print("ERROR: USER variable is not provided by the user.")
     sys.exit(1)
 if not PASSWORD:
-    print('ERROR: Please add your {0} password to 3rd-party credentials in the scheduler-portal under the key :"{1}"'.format(DBMS_PREFIX,CREDENTIALS_KEY))
+    print('ERROR: Please add your database password to 3rd-party credentials in the scheduler-portal under the key :"{0}"'.format(CREDENTIALS_KEY_MSG))
     sys.exit(1)
 if not INPUT_FILE:
     print("ERROR: INPUT_FILE variable is not provided by the user.")
     sys.exit(1)
 if not SQL_TABLE:
-    print("ERROR: {0}TABLE variable is not provided by the user.".format(DBMS_PREFIX))
+    print("ERROR: TABLE variable is not provided by the user.")
     sys.exit(1)
 if not INSERT_MODE:
     INSERT_MODE = 'append'
 
-print("INSERTING DATA IN {0}...".format(DBMS_NAME))
-print(DBMS_PREFIX + 'HOST=' , HOST)
-print(DBMS_PREFIX + 'PORT=' , PORT)
-print(DBMS_PREFIX + 'USER=' , USER)
-print(DBMS_PREFIX + 'DATABASE=' , DATABASE)
-print(DBMS_PREFIX + 'TABLE=' , SQL_TABLE)
+print("INSERTING DATA IN {0} DATABASE...".format(DBMS_NAME))
+print('HOST= ', HOST)
+print('PORT= ', PORT)
+print('USER= ', USER)
+print('DATABASE= ', DATABASE)
+print('TABLE= ', SQL_TABLE)
 
 # Please refer to SQLAlchemy doc for more info about database urls.
 # http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls
-database_url = '{0}+{1}://{2}:{3}@{4}:{5}/{6}'.format(DBMS_PROTOCOL,DBMS_DRIVER,USER,PASSWORD,HOST,PORT,DATABASE)
+database_url = '{0}+{1}://{2}:{3}@{4}:{5}/{6}'.format(DBMS_NAME,DBMS_DRIVER,USER,PASSWORD,HOST,PORT,DATABASE)
 engine = create_engine(database_url)
 dataframe = pd.read_csv(INPUT_FILE, sep='\s+|;|,',index_col=None, engine='python')
 with engine.connect() as conn, conn.begin():
-     dataframe.to_sql(SQL_TABLE, conn, schema=None, if_exists=INSERT_MODE, index=True, index_label=None, chunksize=None, dtype=None)
+    dataframe.to_sql(SQL_TABLE, conn, schema=None, if_exists=INSERT_MODE, index=True, index_label=None, chunksize=None, dtype=None)
 
 print("END Export_Data")
