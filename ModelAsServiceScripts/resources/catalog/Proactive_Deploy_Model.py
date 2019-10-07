302e2d7834fdb3feb114cedbf923d5ff013a785f
Andrews Sobral <andrewssobral@gmail.com>
2019-10-07 18:07:27 +0200

    Improve MAAS scripts

--- ModelAsServiceScripts/resources/catalog/Proactive_Deploy_Model.py
+++ ModelAsServiceScripts/resources/catalog/Proactive_Deploy_Model.py
@@ -1,24 +1,15 @@
-import sys, bz2, uuid, requests, json, pickle
-import pandas as pd
-import numpy as np
-import time
+import os, sys, bz2, uuid, requests, json, pickle, wget, time
+
+def raiser(msg): raise Exception(msg)
 
 MODEL_PATH = "last_model.pkl"
 
-#Get Studio variables
-if 'variables' in locals():
-    if variables.get("MODEL_URL") is not None:
-        model_url = variables.get("MODEL_URL")
-    if variables.get("API_EXTENSION") is not None:
-        api_extension= variables.get("API_EXTENSION")
-    if variables.get("LABEL_COLUMN") is not None:
-        label= variables.get("LABEL_COLUMN")
-
-api_endpoint = variables.get("DEPLOY_MODEL_ENDPOINT") if variables.get("DEPLOY_MODEL_ENDPOINT") else variables.get("ENDPOINT_MODEL")
-api_deploy_endpoint = api_endpoint+api_extension
-print("API_DEPLOY_ENDPOINT",api_deploy_endpoint)
-
-service_token = variables.get("SERVICE_TOKEN") if variables.get("SERVICE_TOKEN") else variables.get("SERVICE_TOKEN_PROPAGATED")
+# Get variables
+API_DEPLOY = variables.get("API_EXTENSION") if variables.get("API_EXTENSION") else raiser("API_EXTENSION is None")
+SERVICE_TOKEN = variables.get("SERVICE_TOKEN") if variables.get("SERVICE_TOKEN") else variables.get("SERVICE_TOKEN_PROPAGATED")
+API_ENDPOINT = variables.get("DEPLOY_MODEL_ENDPOINT") if variables.get("DEPLOY_MODEL_ENDPOINT") else variables.get("ENDPOINT_MODEL")
+API_DEPLOY_ENDPOINT = API_ENDPOINT + API_DEPLOY
+print("API_DEPLOY_ENDPOINT: ", API_DEPLOY_ENDPOINT)
 
 # load the model
 input_variables = {'task.model_id': None}
@@ -31,24 +22,23 @@ for key in input_variables.keys():
 
 model_id = input_variables['task.model_id']
 
-
-if 'variables' in locals():
-    if variables.get(model_id) is not None:
-        model_compressed = variables.get(model_id)
-        model_bin = bz2.decompress(model_compressed)
-        with open(MODEL_PATH, "wb") as f:
-            model = f.write(model_bin)
-        print('model size (original):   ', sys.getsizeof(MODEL_PATH), " bytes")
-        MODEL_PATH = os.path.join(os.getcwd(),MODEL_PATH)
-    else:
-        MODEL_PATH = os.path.join(os.getcwd(),MODEL_PATH)
-        wget.download(model_url,MODEL_PATH)
+if variables.get(model_id) is not None:
+    model_compressed = variables.get(model_id)
+    model_bin = bz2.decompress(model_compressed)
+    with open(MODEL_PATH, "wb") as f:
+        model = f.write(model_bin)
+    print('model size (original):   ', sys.getsizeof(MODEL_PATH), " bytes")
+    MODEL_PATH = os.path.join(os.getcwd(), MODEL_PATH)
+else:
+    MODEL_URL = variables.get("MODEL_URL") if variables.get("MODEL_URL") else raiser("MODEL_URL is None")
+    MODEL_PATH = os.path.join(os.getcwd(), MODEL_PATH)
+    wget.download(MODEL_URL, MODEL_PATH)
 
 final_model = open(MODEL_PATH, 'rb')
-files = {'modelfile':final_model}
-data = {'api_token': service_token}
+files = {'modelfile': final_model}
+data = {'api_token': SERVICE_TOKEN}
 try:
-    req = requests.post(api_deploy_endpoint, files=files, data=data)
+    req = requests.post(API_DEPLOY_ENDPOINT, files=files, data=data)
     print(req.text)
 finally:
     final_model.close()
