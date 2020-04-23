f959b3bee5d0a81492530aff2ee77b10e4a36627
Andrews Sobral <andrewssobral@users.noreply.github.com>
2020-04-23 23:44:15 +0200

    MaaS upgrade, big refactoring, new functionalities, debug and trace mechanism (#623)

--- ModelAsServiceResources/resources/catalog/ml_service.py
+++ ModelAsServiceResources/resources/catalog/ml_service.py
@@ -1,90 +1,169 @@
 #!/usr/bin/env python3
 
-import os, sys, bz2, uuid, pickle, json, connexion, wget
+# import os, sys, bz2, uuid, pickle, json, connexion, wget
+# import numpy as np
+import os
+import glob
+import json
+import connexion
 import pandas as pd
-import numpy as np
 
-from joblib import load
+from datetime import datetime as dt
+from os.path import join, exists, isfile
 from binascii import hexlify
+from shutil import move
 from flask_cors import CORS
+from joblib import load
 
-# models folder
-UPLOAD_MODELS_FOLDER = "model_as_a_service"
+# General parameters
+APP_BASE_DIR = "/model_as_a_service"
+UPLOAD_MODELS_FOLDER = "/model_as_a_service"  # model folder
+CURRENT_MODEL_FILE = join(UPLOAD_MODELS_FOLDER, 'model_last.pkl')  # default model path
+TRACE_FILE = join(UPLOAD_MODELS_FOLDER, 'trace.txt')  # default trace file
+API_KEY = hexlify(os.urandom(16)).decode()  # api key
+TOKENS = {'user': API_KEY}  # user tokens
 
-# load default model
-DEFAULT_MODEL_FILE = os.path.join(UPLOAD_MODELS_FOLDER, 'final_model.pkl')
-global SERVICE_CONFIG
+# Environment variables
+DEBUG_ENABLED = True if (os.getenv('DEBUG_ENABLED') is not None and os.getenv('DEBUG_ENABLED').lower() == "true") else False
+TRACE_ENABLED = True if (os.getenv('TRACE_ENABLED') is not None and os.getenv('TRACE_ENABLED').lower() == "true") else False
+HTTPS_ENABLED = True if (os.getenv('HTTPS_ENABLED') is not None and os.getenv('HTTPS_ENABLED').lower() == "true") else False
 
-# api key
-API_KEY = hexlify(os.urandom(16)).decode() # str(uuid.uuid4())
-# tokens
-TOKENS = {
-  'user'  : API_KEY
-}
 
-# get token api
+def trace(message, token=""):
+    if TRACE_ENABLED:
+        datetime_str = dt.today().strftime('%Y-%m-%d %H:%M:%S')
+        with open(TRACE_FILE, "a") as f:
+            f.write("%s %s %s\n" % (datetime_str, token, message))
+
+
+def log(message, token=""):
+    trace(message, token)
+    if DEBUG_ENABLED:
+        datetime_str = dt.today().strftime('%Y-%m-%d %H:%M:%S')
+        print(datetime_str, token, message)
+    return message
+
+
+def backup_previous_deployed_model():
+    if exists(CURRENT_MODEL_FILE) and isfile(CURRENT_MODEL_FILE):
+        datetime_str = dt.today().strftime('%Y%m%d%H%M%S')
+        PREVIOUS_MODEL_FILE = join(UPLOAD_MODELS_FOLDER, 'model_' + datetime_str + '.pkl')
+        move(CURRENT_MODEL_FILE, PREVIOUS_MODEL_FILE)
+        log("Current model file was moved to:\n" + PREVIOUS_MODEL_FILE)
+
+
 def get_token_api(user) -> str:
-  user = connexion.request.form["user"]
-  token = TOKENS.get(user)
-  if not token:
-    return "Invalid user"
-  else:
-    print('Your token is: {uid}'.format(uid=token))
-    return token
+    user = connexion.request.form["user"]
+    token = TOKENS.get(user)
+    if not token:
+        log('Invalid user: {u}'.format(u=user))
+        return "Invalid user"
+    else:
+        log('{u} token is {t}'.format(u=user, t=token))
+        return token
 
-# predict api
-def predict_api(data: str):
-  api_token = data['api_token']
-  result = ""
-  if api_token == API_KEY:
-    dataframe_json = data['dataframe_json']
-    dataframe = pd.read_json(dataframe_json, orient='values')
-    predictions = predict(dataframe)
-  return json.dumps(list(predictions))
 
-def predict(dataframe):
-  global SERVICE_CONFIG
-  SERVICE_CONFIG = {
-    "model": load(DEFAULT_MODEL_FILE)
-  }
-  model = SERVICE_CONFIG['model']
-  print("model:\n", model)
-  print("dataframe:\n", dataframe)
-  predictions = model.predict(dataframe.values)
-  return predictions
+def predict_api(data: str) -> str:
+    api_token = data['api_token']
+    log("calling predict_api", api_token)
+    if api_token == API_KEY:
+        if exists(CURRENT_MODEL_FILE) and isfile(CURRENT_MODEL_FILE):
+            try:
+                dataframe_json = data['dataframe_json']
+                dataframe = pd.read_json(dataframe_json, orient='values')
+                model = load(CURRENT_MODEL_FILE)
+                log("model:\n" + str(model))
+                log("dataframe:\n" + str(dataframe.head()))
+                predictions = model.predict(dataframe.values)
+                log("Model predictions done", api_token)
+                return json.dumps(list(predictions))
+            except Exception as e:
+                return log(str(e), api_token)
+        else:
+            return log("Model file not found", api_token)
+    else:
+        return log("Invalid token", api_token)
 
-# deploy api
+
 def deploy_api() -> str:
-  api_token = connexion.request.form["api_token"]
-  if api_token == API_KEY:
-    modelfile = connexion.request.files['modelfile']
-    print("modelfile:\n", modelfile)
-    model_id = "final_model"
-    filename = model_id + ".pkl"
-    model_file_path = os.path.join(UPLOAD_MODELS_FOLDER, filename)
-    print("model_file_path:\n", model_file_path)
-    modelfile.save(model_file_path)
-    return "Model deployed"
-  else:
-    return "Invalid Token"
+    api_token = connexion.request.form["api_token"]
+    log("calling deploy_api", api_token)
+    if api_token == API_KEY:
+        backup_previous_deployed_model()
+        model_file = connexion.request.files['model_file']
+        model_file.save(CURRENT_MODEL_FILE)
+        log("The new model file was deployed successfully at:\n" + CURRENT_MODEL_FILE)
+        return log("Model deployed", api_token)
+    else:
+        return log("Invalid token", api_token)
 
-def load_model(model_file_path):
-  import pickle 
-  global SERVICE_CONFIG
-  SERVICE_CONFIG = {
-    "model": load(DEFAULT_MODEL_FILE)
-  }
-  model = pickle.load(open(model_file_path,'rb'))
-  SERVICE_CONFIG['model'] = model
-  print("model:\n", model)
 
-# main
+def list_models_api() -> str:
+    api_token = connexion.request.form["api_token"]
+    log("calling list_models_api", api_token)
+    if api_token == API_KEY:
+        models_list = glob.glob(join(UPLOAD_MODELS_FOLDER, "*.pkl"))
+        log("List of deployed models:\n" + str(models_list))
+        return json.dumps(models_list)
+    else:
+        return log("Invalid token", api_token)
+
+
+def undeploy_api() -> str:
+    api_token = connexion.request.form["api_token"]
+    log("calling undeploy_api", api_token)
+    if api_token == API_KEY:
+        model_file = connexion.request.form["model_file"]
+        if exists(model_file) and isfile(model_file):
+            os.remove(model_file)
+        log("Model removed:\n" + str(model_file))
+        return log("Model removed", api_token)
+    else:
+        return log("Invalid token", api_token)
+
+
+def redeploy_api() -> str:
+    api_token = connexion.request.form["api_token"]
+    log("calling redeploy_api", api_token)
+    if api_token == API_KEY:
+        backup_previous_deployed_model()
+        model_file = connexion.request.form["model_file"]
+        if exists(model_file) and isfile(model_file):
+            move(model_file, CURRENT_MODEL_FILE)
+            log("Model deployed successfully:\n" + str(model_file))
+            return log("Model deployed", api_token)
+        else:
+            return log("Model file not found", api_token)
+    else:
+        return log("Invalid token", api_token)
+
+
+def trace_api() -> str:
+    api_token = connexion.request.form["api_token"]
+    log("calling trace_api", api_token)
+    if api_token == API_KEY:
+        with open(TRACE_FILE) as f:
+            lines = f.readlines()
+        return lines
+    else:
+        return log("Invalid token", api_token)
+
+
 if __name__ == '__main__':
-  app = connexion.FlaskApp(__name__, port=9090, specification_dir='/model_as_a_service')
-  CORS(app.app)
-  app.add_api('ml_service_swagger.yaml', arguments={'title': 'Machine Learning Model Service'})
-  https_enabled = os.getenv('HTTPS_ENABLED')
-  if https_enabled.lower()=="true":
-    app.run(ssl_context=('/model_as_a_service/certificate_mas.pem', '/model_as_a_service/key_mas.pem'))
-  else:
-    app.run()
+    app = connexion.FlaskApp(__name__, port=9090, specification_dir=APP_BASE_DIR)
+    CORS(app.app)
+    app.add_api('ml_service_swagger.yaml', arguments={'title': 'Machine Learning Model Service'})
+
+    
+    if HTTPS_ENABLED:
+        # from OpenSSL import SSL
+        # context = SSL.Context(SSL.SSLv23_METHOD)
+        # context.use_privatekey_file(join(APP_BASE_DIR, 'key_mas.pem'))  # yourserver.key
+        # context.use_certificate_file(join(APP_BASE_DIR, 'certificate_mas.pem'))  # yourserver.crt
+        context = (
+            join(APP_BASE_DIR, 'certificate_mas.pem'),
+            join(APP_BASE_DIR, 'key_mas.pem')
+        )
+        app.run(debug=DEBUG_ENABLED, ssl_context=context)
+    else:
+        app.run(debug=DEBUG_ENABLED)
