5b7725854eb481d1ef8f9490527d9be46f4bd842
Andrews Sobral <andrewssobral@gmail.com>
2020-05-16 02:56:54 +0200

    Improved fork env scripts

--- Scripts/resources/catalog/fork_env_docker_cuda_v4.groovy
+++ Scripts/resources/catalog/fork_env_docker_cuda_v4.groovy
@@ -24,6 +24,14 @@ if ((new File("/.dockerenv")).exists() && ! (new File("/var/run/docker.sock")).e
     println ("Already inside docker container, without host docker access")
     DOCKER_ENABLED = false
 }
+if (DOCKER_ENABLED) {
+    try {
+        Runtime.getRuntime().exec("docker")
+    } catch (Exception e) {
+        println "Docker does not exists : " + e.getMessage()
+        DOCKER_ENABLED = false
+    }
+}
 
 MOUNT_LOG_PATH = variables.get("MOUNT_LOG_PATH")
 DOCKER_LOG_PATH = variables.get("DOCKER_LOG_PATH")
@@ -83,8 +91,34 @@ if (DOCKER_ENABLED) {
     cmd.add("--rm")
     cmd.add("--env")
     cmd.add("HOME=/tmp")
+
     if (CUDA_ENABLED && DOCKER_GPU_ENABLED) {
-        cmd.add("--runtime=nvidia")
+        // Versions earlier than 19.03 require nvidia-docker2 and the --runtime=nvidia flag.
+        // On versions including and after 19.03, you will use the nvidia-container-toolkit package
+        // and the --gpus all flag.
+        try {
+            def sout = new StringBuffer(), serr = new StringBuffer()
+            def proc = 'docker version -f "{{.Server.Version}}"'.execute()
+            proc.consumeProcessOutput(sout, serr)
+            proc.waitForOrKill(1000)
+            docker_version = sout.toString()
+            docker_version = docker_version.substring(1, docker_version.length()-2)
+            docker_version_major = docker_version.split("\\.")[0].toInteger()
+            docker_version_minor = docker_version.split("\\.")[1].toInteger()
+            println "Docker version: " + docker_version
+            if ((docker_version_major >= 19) && (docker_version_minor >= 3)) {
+                cmd.add("--gpus=all")
+            } else {
+                cmd.add("--runtime=nvidia")
+            }
+        } catch (Exception e) {
+            println "Error while getting the docker version: " + e.getMessage()
+            println "DOCKER_GPU_ENABLED is disabled"
+        }
+        // rootless containers leveraging NVIDIA GPUs
+        // needed when cgroups is disabled in nvidia-container-runtime
+        // /etc/nvidia-container-runtime/config.toml => no-cgroups = true
+        cmd.add("--privileged") // https://github.com/NVIDIA/nvidia-docker/issues/1171
     }
 
     String osName = System.getProperty("os.name")
--- Scripts/resources/catalog/fork_env_docker_nvidia_cntk_v2.groovy
+++ Scripts/resources/catalog/fork_env_docker_nvidia_cntk_v2.groovy
@@ -21,6 +21,14 @@ if ((new File("/.dockerenv")).exists() && !(new File("/var/run/docker.sock")).ex
     println ("Already inside docker container, without host docker access")
     DOCKER_ENABLED = false
 }
+if (DOCKER_ENABLED) {
+    try {
+        Runtime.getRuntime().exec("docker")
+    } catch (Exception e) {
+        println "Docker does not exists : " + e.getMessage()
+        DOCKER_ENABLED = false
+    }
+}
 
 DOCKER_GPU_ENABLED = false
 if ("true".equals(variables.get("DOCKER_GPU_ENABLED"))) {
