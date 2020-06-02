4d52ca0a6ef677dc9b00234cbd72a3ec4e7927b2
Andrews Sobral <andrewssobral@users.noreply.github.com>
2020-06-02 22:44:55 +0200

    Improved MaaS prediction call (#671)
    * Improved MaaS prediction call
    
    * Rename Azure workflows
    
    * Rename workflows name and tasks name

--- ModelAsServiceScripts/resources/catalog/Proactive_Call_Prediction_Service.py
+++ ModelAsServiceScripts/resources/catalog/Proactive_Call_Prediction_Service.py
@@ -1,25 +1,34 @@
-import requests, json, bz2
+import bz2
+import json
 import pandas as pd
+import requests
+
 
 def raiser(msg): raise Exception(msg)
 
-# Get variables
-API_PREDICT = variables.get("PREDICT_EXTENSION") if variables.get("PREDICT_EXTENSION") else raiser("PREDICT_EXTENSION is None")
-LABEL_COLUMN = variables.get("LABEL_COLUMN") if variables.get("LABEL_COLUMN") else raiser("LABEL_COLUMN is None")
 
-SERVICE_TOKEN = variables.get("SERVICE_TOKEN") if variables.get("SERVICE_TOKEN") else variables.get("SERVICE_TOKEN_PROPAGATED")
+# -------------------------------------------------------------
+# Get data from the propagated variables
+#
+SERVICE_TOKEN = variables.get("SERVICE_TOKEN") if variables.get("SERVICE_TOKEN") else variables.get(
+    "SERVICE_TOKEN_PROPAGATED")
 assert SERVICE_TOKEN is not None
 
-API_ENDPOINT = variables.get("PREDICT_MODEL_ENDPOINT") if variables.get("PREDICT_MODEL_ENDPOINT") else variables.get("ENDPOINT_MODEL")
+API_PREDICT = variables.get("PREDICT_EXTENSION") if variables.get("PREDICT_EXTENSION") else raiser(
+    "PREDICT_EXTENSION is None")
+API_ENDPOINT = variables.get("PREDICT_MODEL_ENDPOINT") if variables.get("PREDICT_MODEL_ENDPOINT") else variables.get(
+    "ENDPOINT_MODEL")
 assert API_ENDPOINT is not None
 
 API_PREDICT_ENDPOINT = API_ENDPOINT + API_PREDICT
 print("API_PREDICT_ENDPOINT: ", API_PREDICT_ENDPOINT)
 
-# Get data from previous tasks
+INPUT_DATA = variables.get("INPUT_DATA")
+
 input_variables = {
-  'task.dataframe_id_test': None,
-  'task.dataframe_id': None
+    'task.dataframe_id_test': None,
+    'task.dataframe_id': None,
+    'task.label_column': None
 }
 for key in input_variables.keys():
     for res in results:
@@ -28,13 +37,28 @@ for key in input_variables.keys():
             input_variables[key] = value
             break
 
+is_labeled_data = False
+LABEL_COLUMN = variables.get("LABEL_COLUMN")
+if LABEL_COLUMN is not None and LABEL_COLUMN is not "":
+    is_labeled_data = True
+else:
+    LABEL_COLUMN = input_variables['task.label_column']
+    if LABEL_COLUMN is not None and LABEL_COLUMN is not "":
+        is_labeled_data = True
+
+dataframe_columns_name = None
 if input_variables['task.dataframe_id_test'] is not None:
     dataframe_id = input_variables['task.dataframe_id_test']
     dataframe_json = variables.get(dataframe_id)
     dataframe_json = bz2.decompress(dataframe_json).decode()
     assert dataframe_json is not None
     dataframe = pd.read_json(dataframe_json, orient='split')
-    dataframe_test = dataframe.drop([LABEL_COLUMN], axis=1, inplace=False)
+    # dataframe_columns_name = dataframe.columns.values
+    if is_labeled_data:
+        dataframe_test = dataframe.drop([LABEL_COLUMN], axis=1, inplace=False)
+    else:
+        dataframe_test = dataframe.copy()
+    dataframe_columns_name = dataframe_test.columns.values
     dataframe_json = dataframe_test.to_json(orient='values')
 elif input_variables['task.dataframe_id'] is not None:
     dataframe_id = input_variables['task.dataframe_id']
@@ -42,9 +66,14 @@ elif input_variables['task.dataframe_id'] is not None:
     dataframe_json = bz2.decompress(dataframe_json).decode()
     assert dataframe_json is not None
     dataframe = pd.read_json(dataframe_json, orient='split')
-    dataframe_test = dataframe.drop([LABEL_COLUMN], axis=1, inplace=False)
+    # dataframe_columns_name = dataframe.columns.values
+    if is_labeled_data:
+        dataframe_test = dataframe.drop([LABEL_COLUMN], axis=1, inplace=False)
+    else:
+        dataframe_test = dataframe.copy()
+    dataframe_columns_name = dataframe_test.columns.values
     dataframe_json = dataframe_test.to_json(orient='values')
-elif variables.get("INPUT_DATA") is not None:
+elif INPUT_DATA is not None and INPUT_DATA is not "":
     dataframe_json = variables.get("INPUT_DATA")
 else:
     raiser("There is no input data")
@@ -59,24 +88,28 @@ print("predictions:\n", predictions)
 
 predictions = pd.read_json(predictions, orient='records')
 df_dataframe = pd.read_json(dataframe_json, orient='records')
+if dataframe_columns_name is not None:
+    df_dataframe.columns = list(dataframe_columns_name)
 dataframe = df_dataframe.assign(predictions=predictions)
-    
-with pd.option_context('display.max_colwidth', -1):
-  result = dataframe.to_html(escape=False, classes='table table-bordered table-striped', justify='center')
 
+# -------------------------------------------------------------
+# Preview results
+#
+with pd.option_context('display.max_colwidth', -1):
+    result = dataframe.to_html(escape=False, classes='table table-bordered table-striped', justify='center')
 result = """
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Machine Learning Preview</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body class="container">
 <h1 class="text-center my-4" style="color:#003050;">Data Preview</h1>
 <div style="text-align:center">{0}</div>
 </body></html>""".format(result)
-
 result = result.encode('utf-8')
 resultMetadata.put("file.extension", ".html")
 resultMetadata.put("file.name", "output.html")
