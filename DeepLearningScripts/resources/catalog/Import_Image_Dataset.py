209fae8a9b387d16330dd388fc643daf744e09be
Caroline Pacheco <lolyne.pacheco@gmail.com>
2020-06-03 19:15:58 +0200

    Refactored Import_Image_dataset code and improved Search_Train_Image_Classification workflow in the deep learning catalog.

--- DeepLearningScripts/resources/catalog/Import_Image_Dataset.py
+++ DeepLearningScripts/resources/catalog/Import_Image_Dataset.py
@@ -73,6 +73,7 @@ if DATA_PATH is not None and DATA_PATH.startswith("http"):
     remove(filename)
     print("OK")
 else:
+    # load global dataset
     DATASET_PATH = variables.get('DATA_PATH')
     DATASET_NAME = DATASET_PATH
     globalspaceapi.connect()
@@ -84,129 +85,121 @@ else:
 remove_folder = [shutil.rmtree(join(DATASET_PATH, SPLIT_NAME)) for SPLIT_NAME in SPLIT_SETS if exists(join(DATASET_PATH, SPLIT_NAME))]
     
 # load classification dataset
-def load_image_class_dataset():
-    print('Dataset type:', DATASET_TYPE)
+class LoadClassDatset():
 
-    images_val = []
-    labels_val = []
-    folder_name = []
+    def __init__(self):
+        self.folder_name = []
+        self.images_list = []
+        self.label_list = []
+        self.k = 0
 
-    # create image and class lists
-    def create_image_list():
-        k = 0
-        images_list = []
-        label_list = []
+        print('Dataset type:', DATASET_TYPE)
 
+        # create dataset list
         for root in listdir(DATASET_PATH):
             if (not root.startswith('.')):
-                folder_name.append(root)
+                self.folder_name.append(root)
                 label_dir = join(DATASET_PATH, root)
                 print(label_dir)
                 files = listdir(label_dir)
                 files[:] = [join(label_dir, file) for file in files]
                 files_size = len(files)
-                files_label = [k] * files_size
-                images_list = images_list + files
-                label_list = label_list + files_label
-                k += 1
-        return images_list, label_list
+                files_label = [self.k] * files_size
+                self.images_list = self.images_list + files
+                self.label_list = self.label_list + files_label
+                self.k += 1
 
-    # moves images to specific folders
-    def move_images(_path, _images_list, _labels_list, _folder_name):
+        print("Splitting the dataset into train and test")
+        self.img_train, self.img_test, self.lab_train, self.lab_test = train_test_split(self.images_list,
+                                                                                        self.label_list,
+                                                                                        test_size=SPLIT_TEST,
+                                                                                        random_state=1)
+
+    # move images to specific folders
+    def move_images(self, _path, _images_list, _labels_list):
         os.makedirs(_path, exist_ok=True)
-        for _label_name in _folder_name:
+        for _label_name in self.folder_name:
             makedirs(join(_path, _label_name))
         for _image_path, _image_label in zip(_images_list, _labels_list):
-            _image_path_dest = join(_path, _folder_name[_image_label], basename(_image_path))
+            _image_path_dest = join(_path, self.folder_name[_image_label], basename(_image_path))
             print("Moving " + _image_path + " to " + _image_path_dest)
             shutil.move(_image_path, _image_path_dest)
 
-    print("Creating the dataset list")
-    images_list, label_list = create_image_list()
+    def move_split_images(self):
+        if SPLIT_TRAIN != 0.0 and SPLIT_VAL != 0.0:
+            print("Splitting the train into train and val")
+            images_train, images_val, labels_train, labels_val = train_test_split(self.img_train, self.lab_train,
+                                                                                  test_size=SPLIT_VAL, random_state=1)
 
-    print("Splitting the dataset into train and test")
-    images_train, images_test, labels_train, labels_test = train_test_split(images_list, label_list, test_size=SPLIT_TEST, random_state=1)
+        images_split = {SPLIT_SETS[0]: images_train, SPLIT_SETS[1]: images_val, SPLIT_SETS[2]: self.img_test}
+        labels_split = {SPLIT_SETS[0]: labels_train, SPLIT_SETS[1]: labels_val, SPLIT_SETS[2]: self.lab_test}
 
-    if SPLIT_TRAIN != 0.0 and SPLIT_VAL != 0.0:
-        print("Splitting the train into train and val")
-        images_train, images_val, labels_train, labels_val = train_test_split(images_train, labels_train, test_size=SPLIT_VAL, random_state=1)
+        # move images to train, val and test folders
+        for SPLIT_NAME in SPLIT_SETS:
+            SPLIT_PATH = join(DATASET_PATH, SPLIT_NAME)
+            print("SPLIT_PATH:" + SPLIT_PATH)
+            self.move_images(SPLIT_PATH, images_split[SPLIT_NAME], labels_split[SPLIT_NAME])
 
-    images_split = {SPLIT_SETS[0]: images_train, SPLIT_SETS[1]: images_val, SPLIT_SETS[2]: images_test}
-    labels_split = {SPLIT_SETS[0]: labels_train, SPLIT_SETS[1]: labels_val, SPLIT_SETS[2]: labels_test}
+        dataset_labels = json.dumps(self.folder_name)
+        print("DATASET_LABELS: " + dataset_labels)
+        return dataset_labels
 
-    # move images to train, val and test folders
-    for SPLIT_NAME in SPLIT_SETS:
-        SPLIT_PATH = join(DATASET_PATH, SPLIT_NAME)
-        print("SPLIT_PATH:" + SPLIT_PATH)
-        move_images(SPLIT_PATH, images_split[SPLIT_NAME], labels_split[SPLIT_NAME], folder_name)
-
-    dataset_labels = json.dumps(folder_name)
-    print("DATASET_LABELS: " + dataset_labels)
-    return dataset_labels
-
-
+    
 # load detection/segmentation dataset
-def load_image_obj_seg_dataset():
-    print('Dataset type:', DATASET_TYPE)
+class LoadSegObjDatset():
 
-    images_val = []
-    folder_name = ['images', 'classes']
-    folder_number = len(next(os.walk(DATASET_PATH))[1])
+    def __init__(self):
+        self.folder_name = ['images', 'classes']
+        self.folder_number = len(next(os.walk(DATASET_PATH))[1])
+        self.image_dir = join(DATASET_PATH, 'images')
+        self.label_dir = join(DATASET_PATH, 'classes')
+        self.images_list = []
+        self.images_list_gt = []
 
-    # create image and class lists
-    def create_image_list():
-        images_list = []
-        images_list_gt = []
+        print('Dataset type:', DATASET_TYPE)
 
-        if folder_number == 2:
-            image_dir = join(DATASET_PATH, 'images')
-            label_dir = join(DATASET_PATH, 'classes')
+        # create image and class lists
+        if os.path.exists(self.image_dir) and os.path.exists(self.label_dir) and self.folder_number == 2:
+            # get images
+            image_files = listdir(self.image_dir)
+            image_files[:] = [join(self.image_dir, file) for file in image_files]
+            self.images_list = self.images_list + image_files
 
-            if os.path.exists(image_dir) and not os.path.isfile(image_dir):
-                if os.path.exists(label_dir) and not os.path.isfile(label_dir):
-                    image_files = listdir(image_dir)
-                    image_files[:] = [join(image_dir, file)
-                                      for file in image_files]
-                    images_list = images_list + image_files
+            # get classes images
+            label_files = listdir(self.label_dir)
+            label_files[:] = [join(self.label_dir, file) for file in label_files]
+            self.images_list_gt = self.images_list_gt + label_files
 
-                    label_files = listdir(label_dir)
-                    label_files[:] = [join(label_dir, file)
-                                      for file in label_files]
-                    images_list_gt = images_list_gt + label_files
+            # remove .DS_Store files
+            new_list = [self.images_list.remove(''.join(x)) for x in self.images_list if re.search('.DS_Store', x)]
+            new_list_gt = [self.images_list_gt.remove(''.join(x)) for x in self.images_list_gt if
+                           re.search('.DS_Store', x)]
 
-        return images_list, images_list_gt
+            # sort the elements of a given iterable in a specific order (either ascending or descending)
+            self.images_list = sorted(self.images_list)
+            self.images_list_gt = sorted(self.images_list_gt)
+        else:
+            print(
+                "Please check if your dataset is in the correct format! It must contain only 'images' and 'classes' folders")
 
-    # call create_image_list function
-    images_list, images_list_gt = create_image_list()
-    
-    try:
-        new_list = [x for x in images_list if re.search('.DS_Store', x)]
-        images_list.remove(''.join(new_list))
-    except:
-        pass
+        print("Splitting the dataset into train and test")
+        images_train, images_test, images_train_gt, images_test_gt = train_test_split(self.images_list,
+                                                                                      self.images_list_gt,
+                                                                                      test_size=SPLIT_TEST,
+                                                                                      random_state=1)
 
-    try:
-        new_list_gt = [x for x in images_list_gt if re.search('.DS_Store', x)]
-        images_list_gt.remove(''.join(new_list_gt))
-    except:
-        pass
-    
-    images_list = sorted(images_list)
-    images_list_gt = sorted(images_list_gt)
-    
-    
-    print("Splitting the dataset into train and test")
-    images_train, images_test, images_train_gt, images_test_gt = train_test_split(images_list, images_list_gt, test_size=SPLIT_TEST, random_state=1)
+        if SPLIT_TRAIN != 0.0 and SPLIT_VAL != 0.0:
+            print("Splitting the train into train and val")
+            images_train, images_val, images_train_gt, images_val_gt = train_test_split(images_train, images_train_gt,
+                                                                                        test_size=SPLIT_VAL,
+                                                                                        random_state=1)
 
-    if SPLIT_TRAIN != 0.0 and SPLIT_VAL != 0.0:
-        print("Splitting the train into train and val")
-        images_train, images_val, images_train_gt, images_val_gt = train_test_split(images_train, images_train_gt, test_size=SPLIT_VAL, random_state=1)
-
-        images_split = {SPLIT_SETS[0]: images_train, SPLIT_SETS[1]: images_val, SPLIT_SETS[2]: images_test}
-        images_split_gt = {SPLIT_SETS[0]: images_train_gt, SPLIT_SETS[1]: images_val_gt, SPLIT_SETS[2]: images_test_gt}
+            self.images_split = {SPLIT_SETS[0]: images_train, SPLIT_SETS[1]: images_val, SPLIT_SETS[2]: images_test}
+            self.images_split_gt = {SPLIT_SETS[0]: images_train_gt, SPLIT_SETS[1]: images_val_gt,
+                                    SPLIT_SETS[2]: images_test_gt}
 
     # moves images to specific folders
-    def move_seg_images(_path, _images_list, _folder_name):
+    def move_seg_images(self, _path, _images_list):
         os.makedirs(_path, exist_ok=True)
         for _image_path in _images_list:
             _image_path_dest = join(_path, basename(_image_path))
@@ -214,34 +207,32 @@ def load_image_obj_seg_dataset():
             shutil.move(_image_path, _image_path_dest)
 
     # move images to train, val and test folders
-    def move_split_images():
+    def move_split_images(self):
         k = 0
-        for bx in folder_name:
+        dataset_labels = ''
+        for bx in self.folder_name:
             for SPLIT_NAME in SPLIT_SETS:
                 SPLIT_PATH = join(DATASET_PATH, SPLIT_NAME)
                 if k == 0:
                     print("SPLIT_PATH: " + SPLIT_PATH)
-                    move_seg_images(
-                        SPLIT_PATH + '/' + folder_name[0], images_split[SPLIT_NAME], folder_name)
+                    self.move_seg_images(SPLIT_PATH + '/' + self.folder_name[0], self.images_split[SPLIT_NAME])
                 if k == 1:
                     print("SPLIT_PATH: " + SPLIT_PATH)
-                    move_seg_images(
-                        SPLIT_PATH + '/' + folder_name[1], images_split_gt[SPLIT_NAME], folder_name)
+                    self.move_seg_images(SPLIT_PATH + '/' + self.folder_name[1], self.images_split_gt[SPLIT_NAME])
             k += 1
-
-    move_split_images()
-
-    if folder_number != 2:
-        print('Please, check your dataset!')
+        return dataset_labels
 
 
-if DATASET_TYPE == 'classification':
-    DATASET_LABELS = load_image_class_dataset()
-elif DATASET_TYPE == 'detection' or DATASET_TYPE == 'segmentation':
-    DATASET_LABELS = ''
-    load_image_obj_seg_dataset()
-else:
-    print('Please, check your dataset type variable!')
+if __name__ == '__main__':
+    if DATASET_TYPE == 'classification':
+        classdataset = LoadClassDatset()
+        DATASET_LABELS =  classdataset.move_split_images()
+    elif DATASET_TYPE == 'detection' or DATASET_TYPE == 'segmentation':
+        segobjdataset = LoadSegObjDatset()
+        DATASET_LABELS =   segobjdataset.move_split_images()
+    else:
+        print('Please, check your dataset type variable!')
+        
 
 if 'variables' in locals():
     variables.put("DATASET_NAME", DATASET_NAME)
