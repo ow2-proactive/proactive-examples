5e51615601b09c7e3130aab04a881fddd9a6cb2d
Mohamed Khalil LABIDI <47785603+mklkun@users.noreply.github.com>
2020-06-05 10:45:07 +0200

    Increase DBs creation timeout before interaction (#675)

--- DataConnectorScripts/resources/catalog/Parse_Endpoint.groovy
+++ DataConnectorScripts/resources/catalog/Parse_Endpoint.groovy
@@ -7,5 +7,10 @@ def credentialsKey = dbmsName + "://" + user + "@" + host + ":" + port
 variables.put("HOST", host)
 variables.put("PORT", port)
 variables.put("CREDENTIALS_KEY", credentialsKey)
+
+// This value is based on an average estimation of how long it takes handled databases to be up
+// Increase this value if this task fails at first attempt but succeeds at the second.
+def SLEEP_TIME = 11000
+
 // Wait for database sever to be up and fully running.
-sleep(3000)
+sleep(SLEEP_TIME)
