6f1ff9b103c20fe232274503a4eab4c707089015
Imen Bizid <24990300+ImenBizid@users.noreply.github.com>
2020-08-14 15:23:27 +0200

    Fix MaaS PCA links to be accessible via the proxy (#701)
    * Fix MaaS PCA links to be accessible via the proxy
    
    * Update Model_Service.xml
    
    Co-authored-by: Imen BIZID <imen.bizid@activeeon.com>
    Co-authored-by: Andrews Sobral <andrewssobral@users.noreply.github.com>
    (cherry picked from commit 8deda8168a879ad8ed016a9f67cf1c9679da58a9)

--- ModelAsServiceResources/resources/catalog/ml_service.py
+++ ModelAsServiceResources/resources/catalog/ml_service.py
@@ -482,6 +482,7 @@ def trace_preview_api(key) -> str:
                   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
               </head>
                 <body>
+                  <p><a href='ui/#/default' target='_blank'>Click here to access Swagger UI</a></p>
                   <h2 class="text-center my-4" style="color:#003050;">Audit & Traceability</h2>
                   <div style="text-align:center;">{0}
                   <br/>
@@ -492,7 +493,7 @@ def trace_preview_api(key) -> str:
                 </body></html>""".format(config_result, trace_result)
             # Add link to log predictions if enabled
             if get_config('LOG_PREDICTIONS'):
-                result = "<p><a href='/api/predictions_preview?key="+quote(key)+"'>Click here to visualize the predictions</a></p>" + result
+                result = "<p><a href='predictions_preview?key="+quote(key)+"' target='_blank'>Click here to visualize the predictions</a></p>" + result
             return result
         else:
             return log("Trace file is empty", key)
