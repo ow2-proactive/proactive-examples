b9aac1f1960b0d1b2acbf34ef3d5c7fdd70218cd
Andrews Sobral <andrewssobral@gmail.com>
2020-09-03 20:49:26 +0200

    Uses bash4 lowercase mechanism ${variable,,} , set container-no-home disabled by default, fixed Pre_Start_Service

--- PcaScripts/resources/catalog/Pre_Start_Service.groovy
+++ PcaScripts/resources/catalog/Pre_Start_Service.groovy
@@ -1,21 +1,17 @@
 def instanceId = variables.get("PCA_INSTANCE_ID") as long
 def instanceName = variables.get("INSTANCE_NAME")
 def endpointID = variables.get("ENDPOINT_ID")
-    
+def proxified = variables.get("PROXYFIED")
+
 def paSchedulerRestUrl = variables.get('PA_SCHEDULER_REST_URL')
 def pcaUrl = paSchedulerRestUrl.replaceAll("/rest\\z", "/cloud-automation-service")    
 
-proxified = variables.get("PROXYFIED").toLowerCase()
-https_enabled = variables.get("HTTPS_ENABLED").toLowerCase()
-variables.put("PROXYFIED", proxified)
-variables.put("HTTPS_ENABLED", https_enabled)
-
-if (proxified == "true") {
+if ("true".equalsIgnoreCase(proxified)) {
     proxyfiedURL = pcaUrl+"/services/"+instanceId+"/endpoints/"+endpointID+"/"
-	wsURL = proxyfiedURL.replace("https://", "wss://")
-	wsURL = wsURL.replace("http://", "ws://")
-	println "Proxyfied URL :" + proxyfiedURL
-	println "WebSocket URL :" + wsURL
-	variables.put("PROXYFIED_URL", proxyfiedURL)
-	variables.put("WS_PROXYFIED_URL", wsURL)
+    wsURL = proxyfiedURL.replace("https://", "wss://")
+    wsURL = wsURL.replace("http://", "ws://")
+    println "Proxyfied URL :" + proxyfiedURL
+    println "WebSocket URL :" + wsURL
+    variables.put("PROXYFIED_URL", proxyfiedURL)
+    variables.put("WS_PROXYFIED_URL", wsURL)
 }
