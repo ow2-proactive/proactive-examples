2b1a7565dd5e2a93a68b84a7cefca5bbad3932ba
Andrews Sobral <andrewssobral@users.noreply.github.com>
2020-11-24 18:17:50 -0300

    Added conditional SSL context verification to machine-learning-scripts and AutoML (#773)
    * Added unverified SSL context to machine-learning-scripts
    
    * Added unverified SSL context to AutoML
    
    * Added conditional SSL context verification to AutoML
    
    * Added conditional SSL context verification to machine-learning-scripts

--- MachineLearningScripts/resources/catalog/Import_Model.py
+++ MachineLearningScripts/resources/catalog/Import_Model.py
@@ -3,6 +3,7 @@
 
 This module contains the Python script for the Import Model task.
 """
+import ssl
 import urllib.request
 import bz2
 import pickle
@@ -20,7 +21,10 @@ print("BEGIN " + __file__)
 # common utility Python functions and classes
 PA_CATALOG_REST_URL = variables.get("PA_CATALOG_REST_URL")
 PA_PYTHON_UTILS_URL = PA_CATALOG_REST_URL + "/buckets/machine-learning-scripts/resources/Utils/raw"
-exec(urllib.request.urlopen(PA_PYTHON_UTILS_URL).read(), globals())
+if PA_PYTHON_UTILS_URL.startswith('https'):
+    exec(urllib.request.urlopen(PA_PYTHON_UTILS_URL, context=ssl._create_unverified_context()).read(), globals())
+else:
+    exec(urllib.request.urlopen(PA_PYTHON_UTILS_URL).read(), globals())
 global check_task_is_enabled, assert_not_none_not_empty
 
 # -------------------------------------------------------------
