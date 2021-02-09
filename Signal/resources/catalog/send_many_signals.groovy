cba916043fa2d0ecb276d31559a679f71540b062
Nebil Ben Mabrouk <nebil.mabrouk@activeeon.com>
2021-02-09 15:41:03 +0100

    use Set instead of List to handle job signals (#803)

--- Signal/resources/catalog/send_many_signals.groovy
+++ Signal/resources/catalog/send_many_signals.groovy
@@ -2,6 +2,6 @@ import com.google.common.base.Splitter;
 
 signals = args[0]
 
-signalsList = Splitter.on(',').trimResults().omitEmptyStrings().splitToList(signals)
+Set signalsSet = new HashSet<>(Splitter.on(',').trimResults().omitEmptyStrings().splitToList(signals))
 
-signalapi.sendManySignals(signalsList)
+signalapi.sendManySignals(signalsSet)
