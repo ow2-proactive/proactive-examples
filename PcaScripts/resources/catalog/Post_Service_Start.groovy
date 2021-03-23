61efe6e8d69388eb457691f5da982652461b19d8
Mohamed Khalil LABIDI <47785603+mklkun@users.noreply.github.com>
2021-03-23 17:53:44 +0100

    Improve regestering services in PSA management workflows (#853)
    * Improve registering services in PSA management workflows

--- PcaScripts/resources/catalog/Post_Service_Start.groovy
+++
@@ -1,8 +0,0 @@
-def instanceName = variables.get("INSTANCE_NAME")
-def instanceId = variables.get("INSTANCE_ID_" + instanceName)
-def enableActions = args[0]
-
-// Get schedulerapi access and acquire session id
-schedulerapi.connect()
-schedulerapi.registerService(variables.get("PA_JOB_ID").toString(), (int) instanceId, enableActions.toBoolean())
-println("Service instance " + instanceId + "attached to the parent job " + variables.get("PA_JOB_ID"))
