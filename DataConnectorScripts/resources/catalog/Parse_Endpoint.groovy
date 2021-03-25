ee17a5dd8fa30842cd67f979dec07c43e8c802dc
aminelouati <louati.amiine@gmail.com>
2021-03-25 08:50:57 +0100

    Improve database workflows

--- DataConnectorScripts/resources/catalog/Parse_Endpoint.groovy
+++ DataConnectorScripts/resources/catalog/Parse_Endpoint.groovy
@@ -1,12 +1,8 @@
-def dbmsName = args[0]
-def endpoint = new URL(results[0].toString())
-def user = variables.get("USER")
+def endpoint = new URI(results[0].toString())
 def host = endpoint.getHost()
 def port = endpoint.getPort()
-def credentialsKey = dbmsName + "://" + user + "@" + host + ":" + port
 variables.put("HOST", host)
 variables.put("PORT", port)
-variables.put("CREDENTIALS_KEY", credentialsKey)
 
 // This value is based on an average estimation of how long it takes handled databases to be up
 // Increase this value if this task fails at first attempt but succeeds at the second.
