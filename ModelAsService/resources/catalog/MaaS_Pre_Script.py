ae0a693a834b8519894cfd22d8e21aa402fb82ed
Imen Bizid <24990300+ImenBizid@users.noreply.github.com>
2021-11-30 10:54:12 +0100

    Fix maas files downloading from the datalog (#1018)
    Co-authored-by: dianajlailaty <diana.jlailaty@activeeon.com>

--- ModelAsService/resources/catalog/MaaS_Pre_Script.py
+++ ModelAsService/resources/catalog/MaaS_Pre_Script.py
@@ -6,23 +6,15 @@ from cryptography.fernet import Fernet
 
 global schedulerapi, variables
 
-
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-# Install required Python libraries if they are not already installed
+# Import required Python libraries according to Python version
 try:
-    if sys.version_info[0] < 3:
-        from urllib import urlopen
-    else:
-        from urllib.request import urlopen
+    from urllib.request import Request, urlopen  # Python 3
 except ImportError:
-    install('request')
-    if sys.version_info[0] < 3:
-        from urllib import urlopen
-    else:
-        from urllib.request import urlopen
+    from urllib2 import Request, urlopen  # Python 2
 
 try:
     import cryptography
@@ -68,22 +60,26 @@ print("python_file_url: ", python_file_url)
 print("yaml_file_url:   ", yaml_file_url)
 
 # Download the two configuration file "ml_service" for the service definition
+req_py = Request(python_file_url)
+req_py.add_header('sessionid', sessionId)
 if python_file_url.startswith('https'):
     context = ssl._create_unverified_context()
-    python_file = urlopen(python_file_url, context=context).read()
+    python_file = urlopen(req_py, context=context).read()
 else:
-    python_file = urlopen(python_file_url).read()
+    python_file = urlopen(req_py).read()
 python_content = python_file.decode('utf-8')
 python_file_name = PYTHON_ENTRYPOINT + ".py"
 with open(python_file_name, 'w') as f:
     f.write(python_content)
 
 # Download the configuration file "ml_service-api" for the swagger specification
+req_yaml = Request(yaml_file_url)
+req_yaml.add_header('sessionid', sessionId)
 if yaml_file_url.startswith('https'):
     context = ssl._create_unverified_context()
-    yaml_file = urlopen(yaml_file_url, context=context).read()
+    yaml_file = urlopen(req_yaml, context=context).read()
 else:
-    yaml_file = urlopen(yaml_file_url).read()
+    yaml_file = urlopen(req_yaml).read()
 yaml_file_content = yaml_file.decode('utf-8')
 yaml_file_name = YAML_FILE + ".yaml"
 with open(yaml_file_name, 'w') as f:
