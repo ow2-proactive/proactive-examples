71511168f7434e060c1d7fd1d2c8c949b529011a
fviale <fabien.viale@activeeon.com>
2023-05-10 09:48:27 +0200

    Remove usage of check_node_source_name script in AI workflows  - this selection script is superseded by the NODE_SOURCE generic info.  - also renamed check_node_source_name to check_node_source_name_var to avoid confusion with the existing check_node_source script.

--- SelectionScripts/resources/catalog/check_node_source_name_var.groovy
+++ SelectionScripts/resources/catalog/check_node_source_name_var.groovy
@@ -1,3 +1,9 @@
+/**
+ * Script which verifies that the current node runs in the node source specified by the NODE_SOURCE variable
+ *
+ * If the NODE_SOURCE variable is not defined or empty, the script always returns true
+ */
+
 selected = false
 NODE_SOURCE_NAME = variables.get("NODE_SOURCE")
 if (NODE_SOURCE_NAME) {
