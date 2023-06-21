365c51c76ce72569b1b8714c9166c5f1f822c3e0
Mohamed Boussaa <mohamed.boussaa@activeeon.com>
2023-06-21 16:10:39 +0200

    Remove slash from greps

--- PcaScripts/resources/catalog/Resume_Action.bash
+++ PcaScripts/resources/catalog/Resume_Action.bash
@@ -1,6 +1,6 @@
 INSTANCE_NAME=$variables_INSTANCE_NAME
 
-if [ "$(docker ps -a --format '{{.Names}}' | grep "^/$INSTANCE_NAME$")" ]; then
+if [ "$(docker ps -a --format '{{.Names}}' | grep "^$INSTANCE_NAME$")" ]; then
  RUNNING=$(docker inspect --format="{{ .State.Running }}" $INSTANCE_NAME 2> /dev/null)
  STOPPED=$(docker inspect --format="{{ .State.Status }}" $INSTANCE_NAME 2> /dev/null)
 	if [ "$RUNNING" == "true" ]; then
