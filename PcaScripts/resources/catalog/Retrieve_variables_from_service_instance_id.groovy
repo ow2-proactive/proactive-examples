a473ba043e4ed0d66064cf3199756b7d615cbe22
fviale <fabien.viale@activeeon.com>
2023-07-28 15:59:24 +0200

    Fix token format in big data workflows
    Retrieve_variables_from_service_instance_id : allow using variable references with a %{var_name} format
    
    Use PSA_%{INSTANCE_NAME} when used for a token name
    
    Also, change the script type as it is not used in flow scripts.

--- PcaScripts/resources/catalog/Retrieve_variables_from_service_instance_id.groovy
+++ PcaScripts/resources/catalog/Retrieve_variables_from_service_instance_id.groovy
@@ -1,5 +1,6 @@
 import org.ow2.proactive.pca.service.client.ApiClient
 import org.ow2.proactive.pca.service.client.api.ServiceInstanceRestApi
+import org.ow2.proactive.scheduler.common.util.VariableSubstitutor
 
 // Arguments must be <service_id> <var0_name_to_store> <var0_to_retrieve> <var1_name_to_store> <var1_to_retrieve> ...
 if (args.length < 3) {
@@ -27,6 +28,15 @@ def service_instance_rest_api = new ServiceInstanceRestApi(new ApiClient().setBa
 // Retrieve service variables
 def service_instance_data = service_instance_rest_api.getServiceInstanceUsingGET(session_id, service_instance_id)
 for  (i = 1; i < args.length; i = i + 2) {
-    println "[Retrieve_variables_from_service_instance_id] Propagating " + service_instance_data.getVariables().get(args[i+1]) + " under " + args[i]
-    variables.put(args[i], service_instance_data.getVariables().get(args[i+1]))
+    if (args[i+1].contains("%{")) {
+        // var_to_retrieve can contain a reference to another variable in the target service
+        // we use the format %{var} instead of ${var} to prevent conflicts with the current workflow variables
+        def modifiedPattern = args[i+1].replace("%{","\${")
+        def resolvedVariable = VariableSubstitutor.filterAndUpdate(modifiedPattern, service_instance_data.getVariables())
+        println "[Retrieve_variables_from_service_instance_id] Propagating " + resolvedVariable + " under " + args[i]
+        variables.put(args[i], resolvedVariable)
+    } else {
+        println "[Retrieve_variables_from_service_instance_id] Propagating " + service_instance_data.getVariables().get(args[i+1]) + " under " + args[i]
+        variables.put(args[i], service_instance_data.getVariables().get(args[i+1]))
+    }
 }
