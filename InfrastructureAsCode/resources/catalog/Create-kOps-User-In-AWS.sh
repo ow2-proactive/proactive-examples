3d7fc6be35416a05fa2d78cc1520e649cd305084
Nebil Ben Mabrouk <nebil.mabrouk@activeeon.com>
2023-10-03 11:30:40 +0200

    use kOps instead of kops in all files

--- InfrastructureAsCode/resources/catalog/Create-kOps-User-In-AWS.sh
+++ InfrastructureAsCode/resources/catalog/Create-kOps-User-In-AWS.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
+
 USER_NAME=$1
 GROUP_NAME=$2
 
-aws iam get-user --user-name $USER_NAME
-if [ $? -eq 0 ]; then
+USER_EXISTS=1
+aws iam get-user --user-name $USER_NAME || USER_EXISTS=0
+
+if [ $USER_EXISTS -eq 1 ]; then
     echo "User $USER_NAME already exists"
 else
-    aws iam get-group --group-name $GROUP_NAME
-    if [ $? -eq 0 ]; then
-        echo "Group $GROUP_NAME already exists"
-    else
-    	aws iam create-group --group-name $GROUP_NAME
+
+    GROUP_EXISTS=1
+    aws iam get-group --group-name $GROUP_NAME || GROUP_EXISTS=0
+    
+    if [ $GROUP_EXISTS -eq 0 ]; then
+        echo "Creating kOps group $GROUP_NAME ..."
+        aws iam create-group --group-name $GROUP_NAME
     	aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name $GROUP_NAME
     	aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --group-name $GROUP_NAME
     	aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --group-name $GROUP_NAME
     	aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess --group-name $GROUP_NAME
     	aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name $GROUP_NAME
     	aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess --group-name $GROUP_NAME
-    	aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess --group-name $GROUP_NAME
-    	aws iam create-user --user-name $USER_NAME
-    	aws iam add-user-to-group --user-name $USER_NAME --group-name $GROUP_NAME
-    	aws iam create-access-key --user-name $USER_NAME
+    	aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess --group-name $GROUP_NAME        
     fi
+    echo "Creating kOps user $USER_NAME ..."
+    aws iam create-user --user-name $USER_NAME
+    aws iam add-user-to-group --user-name $USER_NAME --group-name $GROUP_NAME
+    aws iam create-access-key --user-name $USER_NAME
+
 fi
