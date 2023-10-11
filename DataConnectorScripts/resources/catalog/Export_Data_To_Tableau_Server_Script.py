e636abd8658f1beea243efbd7dd094d581d6485b
Andrews Cordolino Sobral <andrewssobral@gmail.com>
2023-10-11 10:32:49 +0200

    Fixed Tableau workflows

--- DataConnectorScripts/resources/catalog/Export_Data_To_Tableau_Server_Script.py
+++ DataConnectorScripts/resources/catalog/Export_Data_To_Tableau_Server_Script.py
@@ -7,10 +7,11 @@ import pandas as pd
 import pantab
 import bz2
 
-#connexion to tableau server
-#Add your credentials in the schedular portal
-USERNAME=credentials.get("TABLEAU_SERVER_USERNAME")
-PASSWORD=credentials.get("TABLEAU_SERVER_PASSWORD")
+# Connection to the Tableau Server
+# Create an account at https://www.tableau.com/products/server
+# Add your credentials in the schedular portal
+USERNAME = credentials.get("TABLEAU_SERVER_USERNAME")
+PASSWORD = credentials.get("TABLEAU_SERVER_PASSWORD")
 
 SERVER_ENDPOINT = variables.get("SERVER_ENDPOINT")
 SITE_ID = variables.get("SITE_ID")
@@ -18,24 +19,23 @@ PROJECT_NAME = variables.get("PROJECT_NAME")
 INPUT_FILE_NAME = variables.get("INPUT_FILE_NAME")
 
 tableau_auth = TSC.TableauAuth(USERNAME, PASSWORD, site_id=SITE_ID)
-#tableau_auth = TSC.TableauAuth('lolyne.pacheco@gmail.com', 'proactive123', site_id='carolinetableautest')
 server = TSC.Server(SERVER_ENDPOINT)
 project_id = None
 
 with server.auth.sign_in(tableau_auth):
     all_project_items, pagination_item = server.projects.get()
-    #check if the specified project_name exists
+    # Check if the specified project_name exists
     for project in all_project_items:
         if project.name == PROJECT_NAME:
-            project_id=project.id 
+            project_id = project.id 
     if project_id is None:
-        # create a new project item
-    	new_project = TSC.ProjectItem(name=PROJECT_NAME, content_permissions='LockedToProject', id='LockedToProject')
-    	# create the project 
-    	new_project = server.projects.create(new_project)
-    	# Create new datasource_item with project id '3a8b6148-493c-11e6-a621-6f3499394a39'
-        
-    #save dataframe into a .hyper file
+        # Create a new project item
+        new_project = TSC.ProjectItem(name=PROJECT_NAME, content_permissions='LockedToProject', id='LockedToProject')
+        # Create the project 
+        new_project = server.projects.create(new_project)
+        # Create new datasource_item with project id '3a8b6148-493c-11e6-a621-6f3499394a39'
+
+    # Save dataframe into a .hyper file
     input_variables = {'task.dataframe_id': None}
     for key in input_variables.keys():
       for res in results:
@@ -53,7 +53,7 @@ with server.auth.sign_in(tableau_auth):
 
     dataframe = pd.read_json(dataframe_json, orient='split')
     pantab.frame_to_hyper(dataframe, INPUT_FILE_NAME)
-    
+
     # Create new datasource_item
     new_datasource = TSC.DatasourceItem(project_id)
     new_datasource = server.datasources.publish(new_datasource, INPUT_FILE_NAME, 'CreateNew')
