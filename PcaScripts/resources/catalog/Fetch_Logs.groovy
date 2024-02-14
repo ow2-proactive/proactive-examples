8cec8e55395b6809bd3bcb49aee358d669e609dd
aminelouati <louati.amiine@gmail.com>
2024-02-14 22:27:14 +0100

    Fix variable initialization

--- PcaScripts/resources/catalog/Fetch_Logs.groovy
+++ PcaScripts/resources/catalog/Fetch_Logs.groovy
@@ -1,6 +1,8 @@
 // Check if loop task has ordered to finish the loop
-def isFinished = variables.get('IS_FINISHED').toBoolean()
-loop = isFinished ? false : '*/1 * * * *'
+def isFinished = false
+if (variables.get("IS_FINISHED")) {
+    isFinished = variables.get("IS_FINISHED").toBoolean()
+}
 
 // Set a time marker to fetch logs since this marker.
 variables.put("LAST_TIME_MARKER",new Date().format("yyyy-MM-dd'T'HH:mm:ssXXX"))
